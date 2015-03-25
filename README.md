99a8ce72070b6d1bc9c77c789aef09e2a2ff0f56
David Von Lehman <david.vonlehman@nordstrom.com>
2015-03-25 15:00:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 4front-dynamo
+
+DynamoDB based metadata repository for 4front web platform. The metadata repository is used to persist information about virtual apps, organizations, users, app versions, environments, environment variables, and more.
---
+++ scripts/create-local-tables.js
@@ -0,0 +1,45 @@
+// Script to drop and recreate all of the tables in a local dynamodb for unit tests.
+// http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Tools.DynamoDBLocal.html
+
+var _ = require('lodash'),
+  util = require('util'),
+  async = require('async'),
+  vogels = require('vogels'),
+  AWS = require('aws-sdk'),
+  modelDefinitions = require('../lib/models');
+
+// Connect to DynamoDB local
+var dynamoDb = new AWS.DynamoDB({ 
+  region: 'us-west-2',
+  endpoint: 'http://localhost:8000'
+});
+
+vogels.dynamoDriver(dynamoDb);
+
+async.each(_.keys(modelDefinitions), function(type, cb) {
+  var defn = modelDefinitions[type];
+
+  console.log("Deleting and recreating table " + defn.tableName);
+  dynamoDb.deleteTable({TableName: defn.tableName}, function(err, data) {
+    if (err && /ResourceNotFoundException/.test(err.toString()) === false)
+      return cb(err);
+
+    var model = vogels.define(type, defn);
+    model.createTable({}, function(err) {
+      if (err) {
+        if (/ResourceInUseException/.test(err.toString()) === true) {
+          console.log("Table " + defn.tableName + " already exists");
+          return cb(null);
+        }
+        else
+          return cb(err);
+      }
+      cb();
+    });
+  });
+}, function(err) {
+  if (err)
+    return console.log("Error creating table: " + err);
+
+  console.log("Done creating tables");
+});
